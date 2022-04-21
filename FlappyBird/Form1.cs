b469dfc1b251a7ee2f2037f8e2b2e0a8ba0e6b8a
yandutka@mail.ru <yandutka@mail.ru>
2022-04-21 21:19:56 +0300

    Add project files.

---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        bool gameStart = false;
+        int gravity = -3;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+        void MoveTube1()
+        {
+            while (gameStart)
+            {
+                pictureBox1.Invoke(new Action(() => pictureBox1.Left -= 3));
+
+                if (pictureBox1.Left < -pictureBox1.Width)
+                    pictureBox1.Invoke(new Action(()
+                        => {
+                                pictureBox1.Left = Width +
+                                random.Next(pictureBox1.Width, pictureBox1.Width + 100);
+
+                                pictureBox1.Top = 
+                                random.Next(Height / 2, Height / 2 + pictureBox1.Height / 2);
+                           }));
+                Thread.Sleep(5);
+            }
+        }
+
+        void MoveTube2()
+        {
+            while(gameStart)
+            {
+                pictureBox2.Invoke(new Action(() => pictureBox2.Left -= 3));
+
+                if (pictureBox2.Left < -pictureBox2.Width)
+                    pictureBox2.Invoke(new Action(()
+                       => {
+                            pictureBox2.Left = Width + 
+                            random.Next(pictureBox2.Width, pictureBox2.Width + 100);
+
+                            pictureBox2.Top =
+                            random.Next(-pictureBox2.Height /2, -10);
+                        }));
+                Thread.Sleep(5);
+            }
+        }
+
+        void Gravity()
+        {
+            while(gameStart)
+            {
+                if(pictureBox3.Top <= 0 || pictureBox3.Top >= Height-100)
+                {
+                    pictureBox3.Invoke(
+                    new Action(() => pictureBox3.Top = Height / 2));
+                    gameStart = false;
+                }
+                if (gravity < 3) gravity++;
+                pictureBox3.Invoke(
+                    new Action(() =>
+                    {
+                        pictureBox3.Top += gravity;
+                    }));
+                Thread.Sleep(3);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameStart = true;
+            Thread tube1 = new Thread(MoveTube1);
+            Thread tube2 = new Thread(MoveTube2);
+            Thread grav = new Thread(Gravity);
+            tube1.Start();
+            tube2.Start();
+            grav.Start();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            gravity = -12;
+        }
+    }
+}
---
+++ NewForm/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NewForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Thread thread; //Создаём поток
+        void ProgressChangeding()
+        {
+            while(progressBar1.Value < progressBar1.Maximum)
+            {
+                //Лямба-выражение - это создание функции во время выполнения другой функции
+                progressBar1.Invoke(new Action(() =>    //Метод invoke позволяет работать с этим потоком
+                                                        //из других потоков
+                {
+                    progressBar1.Value += 1;
+                }));
+                Thread.Sleep(50);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(thread == null || thread.IsAlive == false)   //Если поток пустой или "мёртвый"
+            {
+                progressBar1.Value = 0;
+                thread = new Thread(ProgressChangeding); //Инициализируем поток тем методом, который хотим
+                                                         //чтобы выполнялся в отдельном потоке
+                thread.Start();     //Запускаем поток
+                //thread.Abort();   //Использовать не стоит уже
+            }
+        }
+
+        
+    }
+}
